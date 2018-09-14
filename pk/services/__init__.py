8dafed18de85ab29dcf106593595527e49145b09
tulare <tulare.paxgalactica@gmail.com>
2018-09-14 11:44:14 +0200

    complete setup for __version__, wheel and install_requires

--- pk/services/__init__.py
+++ pk/services/__init__.py
@@ -1,4 +1,4 @@
 # -*- encoding: utf-8 -*-
 
-__version__ = '0.1.0'
+from .version import __version__
 
--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@
 
 from setuptools import setup, find_packages
 
+# Get version without import module
+exec(compile(open('pk/services/version.py').read(),
+             'pk/services/version.py', 'exec'))
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # https://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-dependencies
+    'youtube-dl>=2018.9',
+]
+
 with open('README.md') as f :
     readme = f.read()
 
@@ -10,7 +21,7 @@ with open('LICENSE') as f :
 
 setup(
     name='pk.services',
-    version='0.1.0',
+    version=__version__,
     description='Various services for python',
     long_description=readme,
     author='Tulare Regnus',
@@ -20,4 +31,5 @@ setup(
     packages=find_packages(exclude=('tests',)),
     namespace_packages=['pk'],
     zip_safe=False,
+    install_requires=install_requires
 )
